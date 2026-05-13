7df5d34b93bbc1bd91597b16054c90ad9594a5f6
Robert Sanders <rsanders@pepperdash.com>
2026-05-13 16:17:21 -0400

    fix: changed the 'unknown response' debug level to verbose

--- src/PowerHandler.cs
+++ src/PowerHandler.cs
@@ -93,7 +93,7 @@ namespace EpsonProjectorEpi
                 return;
             }
 
-            this.LogWarning("Received an unknown power response: {response}", response);            
+            this.LogVerbose("Received an unknown power response: {response}", response);
         }
 
         private void OnPowerUpdated(Events.PowerEventArgs args)
